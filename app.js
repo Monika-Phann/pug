03c943cde337c06af6cab563dc9dfa613acc2e73
AnB School 0017 <anbschool0017@AnBs-Mac-mini-135.local>
2025-09-26 18:48:38 +0700

    handlebars

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
 // Set Pug as the view engine
-app.set('view engine', 'pug');
+app.engine('hbs', engine({extname: 'hbs'}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 const adminData = require('./routes/admin');
@@ -18,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', layout: false });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
