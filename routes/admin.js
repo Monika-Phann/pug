f66f8e6dff77faff987685adabd80473b42662be
AnB School 0017 <anbschool0017@AnBs-Mac-mini-135.local>
2025-09-29 11:47:25 +0700

    hbs

--- routes/admin.js
+++ routes/admin.js
@@ -8,13 +8,18 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', productCSS: true, activeAddproduct: true});
 });
 
 router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({
+    title: req.body.title,
+    price: req.body.price || '$599',      // add price field
+    imageUrl: req.body.imageUrl || 'https://www.apple.com/newsroom/images/product/watch/lifestyle/Apple-Watch-SE-aluminum-silver-220907_inline.jpg.large.jpg'
+  });
   res.redirect('/');
 });
+
 
 exports.routes = router;
 exports.products = products; 
--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,20 @@
 const path = require('path');
-
 const express = require('express');
-
 const rootDir = require('../util/path');
-const adminData = require('./admin');
+
+const adminData = require('./admin'); // expects admin.js to export the products array (e.g. module.exports.products = products;)
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {prods: products, docTitle: 'Shop', path:'/'});
+  res.render('shop', {
+    pageTitle: 'Shop',
+    path: '/',
+    prods: adminData.products,          // <-- pass products to template
+    hasProducts: adminData.products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
